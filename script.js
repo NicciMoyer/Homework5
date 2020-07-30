87b5ea45f1632e3cc733859bdd14538d1366d29b
Nicci Moyer <niccihendrix@yahoo.com>
2020-07-29 20:07:47 -0500

    structure

---
+++ script.js
@@ -0,0 +1,27 @@
+$("#currentTime").text(moment().format('MMMM Do YYYY, h:mm a'))
+
+// function currentTime() {
+//     let hourCurrent = moment().format("h")
+//     let valueTime = $(".col-md-1");
+//     valueTime.value
+
+    $row.addClass("past")
+    
+    
+    // if (currentTime = valueTime) {
+    //     $("row").addClass("present", function () { })
+         
+    // } else if (currentTime > valueTime) {
+    //     $("row").addClass("past", function () {
+            
+    //     })
+        
+    // } else {
+    //     $("row").addClass("future", function () {
+            
+    //     })
+    // }
+    
+    // console.log(valueTime)
+// }
+// currentTime()
