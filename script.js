ae209ac5e6ecea8e48ee74411c4b05d779bc018a
Nicci Moyer <niccihendrix@yahoo.com>
2020-07-30 20:07:18 -0500

    times, buttons working

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
 $("#currentTime").text(moment().format('MMMM Do YYYY, h:mm a'))
+let hourCurrent = moment().hour
+let $centerCol = $(".centerCol")
 
-// function currentTime() {
-//     let hourCurrent = moment().format("h")
-//     let valueTime = $(".col-md-1");
-//     valueTime.value
+currentTime()
+function currentTime() {
+    
+    let valueTime = $(".col-md-10");
 
-    $row.addClass("past")
+
+    valueTime.value
+
     
+
+   for (let i = 0; i < $centerCol.length; i++) {
+       const loopingTime = $centerCol[i];
+
+       if ($(".centerCol")[i].getAttribute("value")===hourCurrent) {
+           $(".row").addClass("present")
+       } 
+       else if ($(".centerCol")[i].getAttribute("value")>hourCurrent){
+           $(".row").addClass("future")
+       }
+       else if ($(".centerCol")[i].getAttribute("value")<hourCurrent) {
+           $(".row").addClass("past")
+       }   
+   }
+}
+
+function insertNote  () {
+
+    $("#centerCol").empty();
+
+    for (var i = 0; i < notes.length; i++) {
+
+      var a = $("<button>");
+ 
+      a.addClass("");
     
-    // if (currentTime = valueTime) {
-    //     $("row").addClass("present", function () { })
-         
-    // } else if (currentTime > valueTime) {
-    //     $("row").addClass("past", function () {
-            
-    //     })
-        
-    // } else {
-    //     $("row").addClass("future", function () {
-            
-    //     })
-    // }
-    
-    // console.log(valueTime)
-// }
-// currentTime()
+      a.attr("data-name", notes[i]);
+     
+      a.text(notes[i]);
+   
+      $("#centerCol").append(a);
+    }
+  }
+
+  $("#add-note").on("click", function(event) {
+
+    event.preventDefault();
+
+    var note = $("#note-input").val().trim();
+
+    notes.push(note);
+
+    insertNote ();
+  });
+
+  insertNote ();
